39bfc5c3b879abb313e74183d09ded9c50e0ac39
muramariz23 <m.rizkiramadhan2003@gmail.com>
2021-08-04 06:06:29 +0700

    menambah halaman tampilan

---
+++ iot-app/resources/views/scan.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Scanning</h1>
+</body>
+</html>
--- iot-app/routes/web.php
+++ iot-app/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/data_siswa', function () {
+    return view('datasiswa');
+});
+
+Route::get('/rekap_absensi', function () {
+    return view('rekapabsen');
+});
+
+Route::get('/scan', function () {
+    return view('scan');
+});
